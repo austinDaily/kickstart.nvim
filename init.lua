0398307d9b9f61dc29506562715a86042e07b2e6
austinDaily <snowtheparrot@proton.me>
2025-07-31 19:42:18 -0400

    configured neotree and set my lsp's

--- init.lua
+++ init.lua
@@ -585,11 +585,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        elixirls = {},
-        ruby_lsp = {},
-        -- clangd = {},
-        -- gopls = {},
-        --
+        ts_ls = {},
+        -- gleam
+        --require('lspconfig').gleam.setup {},
 
         lua_ls = {
           -- cmd = { ... },
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,17 +1,38 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
+-- Neo-tree configuration with toggle and focus keybinds
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons', -- recommended
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    {
+      '<Space>t',
+      function()
+        require('neo-tree.command').execute { toggle = true, dir = vim.loop.cwd() }
+      end,
+      desc = 'NeoTree Toggle',
+      silent = true,
+    },
+    {
+      '<Space>f',
+      function()
+        local state = require('neo-tree.sources.manager').get_state 'filesystem'
+        if state and state.current_position then
+          if vim.api.nvim_get_current_win() == state.winid then
+            vim.cmd.wincmd 'p' -- go to previous window
+          else
+            vim.api.nvim_set_current_win(state.winid)
+          end
+        end
+      end,
+      desc = 'NeoTree Focus Toggle',
+      silent = true,
+    },
   },
   opts = {
     filesystem = {
